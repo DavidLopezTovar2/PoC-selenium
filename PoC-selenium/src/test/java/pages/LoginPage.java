ecfacf1f7f6055799d3b1d5276ccb6f9127b26aa
David López <dalopez@javeriana.edu.co>
2022-06-06 12:30:39 -0500

    Login test with TestNG

---
+++ PoC-selenium/src/test/java/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends PageBase {
+
+    public LoginPage (WebDriver driver){
+        super(driver);
+    }
+
+
+
+}
